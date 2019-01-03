182e37c814a96cde4e02298986ee887aa577a68b
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 19:34:44 +0700

    Add files via upload

---
+++ Program memvalidasi umur.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int usia;
+    cout<<"program memvalidasi umur"<<endl;
+    cout<<"masukan usia(1 s/d 100)"<<endl;
+    cin>>usia;
+    if(usia<50)
+        cout<<"anak anak, remaja,dan dewasa"<<endl;
+    return 0;
+}
